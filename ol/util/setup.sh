c09b1f2fdb8ea4197b336e89277e0d0dae2a972f
roamingrabbit <0L@hophop.nom>
2021-05-06 22:08:09 -0400

    update validator docs and setup commands.
    * updating docs after new code from release 4.3.0.
    * fixing path in Makefile.
    * setup should run apt as sudo so the entire script doesn't need to be run as root.

--- ol/util/setup.sh
+++ ol/util/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # targeting ububtu
-apt update
-apt install -y git vim zip jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld
+sudo apt update
+sudo apt install -y git vim zip jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+export PATH="$HOME/.cargo/bin:$PATH"
 cargo install toml-cli
 cargo install sccache
 
